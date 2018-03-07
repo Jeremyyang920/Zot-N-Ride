9abb8064059c3e4f4c8bf08f22a480b6cf64b720
Jeremy Yang <jeremy2@uci.edu>
2018-03-06 20:37:19 -0800

    Created login endpoint with logging

--- python/app/__init__.py
+++ python/app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 from app import routes
-bcrypt=Bcrypt(app)
+
--- python/app/routes.py
+++ python/app/routes.py
@@ -6,6 +6,7 @@ from flask import jsonify
 import sys
 sys.path.append('/../')
 import zot_n_ride as ZNR
+import bcrypt
 
 @app.route('/')
 @app.route('/home')
@@ -17,19 +18,26 @@ def welcome():
 def display_profile(username):
     return 'User {}'.format(username)
 
+#Endpoint for creating a user
 @app.route('/api/register',methods=['POST'])
 def register_user():
     if not request.json:
         abort(400)
     body=request.json
-    query=ZNR.create_user_into_db(body['netID'],body['password'],body['firstname'],body['lastname'],body['major'],body['address'],body['isDriver'])
+    hashedPW=bcrypt.hashpw(body['password'].encode('utf-8'),bcrypt.gensalt(12))
+    query=ZNR.create_user_into_db(body['email'],hashedPW,body['firstname'],body['lastname'],body['major'],body['address'],body['isDriver'])
     if(query == None):
         abort(400)
     return json.dumps(request.json)
 
+#Endpoint for validating a login
 @app.route('/api/login',methods=['POST'])
 def login_user():
     if not request.json:
         abort(400)
     body=request.json
+    query=ZNR.validate_login(body['email'],body['password'])
+    if(not query):
+        abort(400)
+    return json.dumps(request.json)
     
