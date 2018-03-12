26d3d080fea5dd64bc8207a3ada4702783c38505
Jeremy Yang <jeremy2@uci.edu>
2018-03-12 15:14:53 -0700

    Update app and change directories

---
+++ app.py
@@ -0,0 +1,46 @@
+from app import app
+from flask import Flask,abort,request,Response
+import json
+from flask import jsonify
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+import sys
+import zot_n_ride as ZNR
+import bcrypt
+
+@app.route('/')
+@app.route('/home')
+@app.route('/welcome')
+def welcome():
+    return 'Welcome to Zot n\' Ride!'
+
+@app.route('/user/<username>')
+def display_profile(username):
+    return 'User {}'.format(username)
+
+#Endpoint for creating a user
+@app.route('/api/register',methods=['POST'])
+def register_user():
+    if not request.json:
+        abort(400)
+    body=request.json
+    hashedPW=bcrypt.hashpw(body['password'].encode('utf-8'),bcrypt.gensalt(12))
+    query=ZNR.create_user_into_db(body['email'],hashedPW,body['firstname'],body['lastname'],body['major'],body['address'],body['isDriver'])
+    if(query == None):
+        abort(400)
+    return json.dumps(request.json)
+
+#Endpoint for validating a login
+@app.route('/api/login',methods=['POST'])
+def login_user():
+    if not request.json:
+        abort(400)
+    body=request.json
+    query=ZNR.validate_login(body['email'],body['password'])
+    if(not query):
+        abort(400)
+    return json.dumps(request.json)
+    
